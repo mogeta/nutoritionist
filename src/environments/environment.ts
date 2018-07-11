209d1f0409b1f5fc38ca3a6264d58e24e8af202d
mogge <muzigen@gmail.com>
2018-07-12 07:57:29 +0900

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAdOLczN6Dqyz-lKZgbwy5ohNFHQQJl83g",
+    authDomain: "builder-206303.firebaseapp.com",
+    databaseURL: "https://builder-206303.firebaseio.com",
+    projectId: "builder-206303",
+    storageBucket: "builder-206303.appspot.com",
+    messagingSenderId: "813500523440"
+  }
 };
 
 /*
