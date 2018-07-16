099266053035648574247556f92bf19fe77eadc0
mogge <muzigen@gmail.com>
2018-07-17 07:49:25 +0900

    add routing daily report

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {StressSliderComponent} from "./stress-slider/stress-slider.component";
 import {LoginButtonComponent} from "./login-button/login-button.component";
+import {DailyReportComponent} from "./daily-report/daily-report.component";
 
 const heroesRoutes: Routes = [
   { path: 'heroes', redirectTo: 'stress' },
   { path: 'stress', component: StressSliderComponent },
+  { path: 'report', component: DailyReportComponent },
   { path: '**', component: LoginButtonComponent }
 ];
 
