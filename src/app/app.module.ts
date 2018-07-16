9faee2e2c5cddbb1eba6f00c5769f3464acd3839
mogge <muzigen@gmail.com>
2018-07-17 07:48:33 +0900

    create daily report

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,14 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSliderModule}   from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './/app-routing.module';
+import { DailyReportComponent } from './daily-report/daily-report.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginButtonComponent,
-    StressSliderComponent
+    StressSliderComponent,
+    DailyReportComponent
   ],
   imports: [
     BrowserModule,
