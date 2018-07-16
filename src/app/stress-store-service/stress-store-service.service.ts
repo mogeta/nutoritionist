204f1a2119996f949bc1a63b2536e0ca9a26c0aa
mogge <muzigen@gmail.com>
2018-07-16 15:23:47 +0900

    update tslint

--- src/app/stress-store-service/stress-store-service.service.ts
+++ src/app/stress-store-service/stress-store-service.service.ts
@@ -1,25 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {AngularFirestore} from "angularfire2/firestore";
-import { firebase } from '@firebase/app';
+import {firebase} from '@firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StressStoreServiceService {
 
-  constructor(public db: AngularFirestore) { }
+  constructor(public db: AngularFirestore) {
+  }
 
-  public Add(value){
+  public Add(value) {
     const timestamp = firebase.firestore.FieldValue.serverTimestamp();
     return this.db.collection("stress").add({
       created_at: timestamp,
       parameter: value
     });
-      // .then(function(docRef) {
-      //   console.log("Document written with ID: ", docRef.id);
-      // })
-      // .catch(function(error) {
-      //   console.error("Error adding document: ", error);
-      // });
   }
 }
