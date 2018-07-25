952f53d133e71ba9cff420d77562dc77ca5388ee
mogge <muzigen@gmail.com>
2018-07-26 07:16:03 +0900

    update firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+
 export const environment = {
-  production: true
+	production: true,
+	firebase: {
+		apiKey: 'AIzaSyAdOLczN6Dqyz-lKZgbwy5ohNFHQQJl83g',
+		authDomain: 'builder-206303.firebaseapp.com',
+		databaseURL: 'https://builder-206303.firebaseio.com',
+		projectId: 'builder-206303',
+		storageBucket: 'builder-206303.appspot.com',
+		messagingSenderId: '813500523440'
+	}
 };
